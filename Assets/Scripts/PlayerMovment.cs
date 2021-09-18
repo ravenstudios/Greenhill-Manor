cb361fa8912044934f1948bb82059b19b38b24a5
ravenstudios <ravenstudiosrob@yahoo.com>
2021-09-18 00:59:00 -0500

    working on animation

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -6,43 +6,54 @@ using UnityEngine.InputSystem;
 public class PlayerMovment : MonoBehaviour
 {
     [SerializeField]
-    float _walkSpeed = 5f;
+    float _walkSpeed, _jumpSpeed, _rayCastLength;
 
     [SerializeField]
+    bool _isJumping, _isFacingRight, _isOnGround;
+
     
-    bool _isJumping = false;
-    [SerializeField]
-    float _jumpSpeed = 5f;
-
 
     [SerializeField]
     Rigidbody2D rb;
 
+
+    [Header("Collision")]
+    [SerializeField]
+    Vector3 _rayCastOffset;
+
+
+
+
+
+
     InputActions _inputActions;
 
-    private void Awake()
+    void Awake()
     {
         _inputActions = new InputActions();
         _inputActions.Player.Jump.performed += ctx => StartJump();
         _inputActions.Player.Jump.canceled += ctx => EndJump();
+
     }
 
-    private void OnEnable()
+    void Start()
     {
-        _inputActions.Enable();
+        
     }
 
-    private void OnDisable()
+    void OnMove()
     {
-        _inputActions.Disable();
+        FlipAnimation();
     }
 
-    
+
+
 
     void StartJump()
     {
         rb.velocity = new Vector2(rb.velocity.x, 0);
         _isJumping = true;
+
 
     }
 
@@ -51,27 +62,51 @@ public class PlayerMovment : MonoBehaviour
         _isJumping = false;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    void Jump() => rb.AddForce(Vector2.up * _jumpSpeed * Time.deltaTime, ForceMode2D.Impulse);
-
-    // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 direction = _inputActions.Player.Move.ReadValue<Vector2>();
-        float walking = direction.x * _walkSpeed;
-        
+        if(!_isOnGround)
+        {
+
+        }
+
+
+        Walk();
         if (_isJumping)
         {
             Jump();
         }
 
-        
+    }
+
+
+    void Walk()
+    {
+        Vector2 direction = _inputActions.Player.Move.ReadValue<Vector2>();
+        float walking = direction.x * _walkSpeed;
         rb.AddForce(new Vector2(direction.x * _walkSpeed, rb.position.y));
-        
+    }
+
+
+    void Jump() => rb.AddForce(Vector2.up * _jumpSpeed * Time.deltaTime, ForceMode2D.Impulse);
+    void OnEnable() => _inputActions.Enable();
+    void OnDisable() => _inputActions.Disable();
+
+
+    void FlipAnimation()
+    {
+        _isFacingRight = !_isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position + _rayCastOffset, transform.position + _rayCastOffset + Vector3.down * _rayCastLength);
+        Gizmos.DrawLine(transform.position - _rayCastOffset, transform.position - _rayCastOffset + Vector3.down * _rayCastLength);
+
     }
 }
