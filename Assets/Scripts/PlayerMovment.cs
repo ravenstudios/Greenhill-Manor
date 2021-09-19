8b35263c5da80ed6054491b1f66f684248cb7e13
Robert Dodson <ravenstudiosrob@yahoo.com>
2021-09-19 02:54:21 -0500

    refractored code

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -45,37 +45,22 @@ public class PlayerMovment : MonoBehaviour
 
     }
 
-    void Start()
-    {
-        
-    }
-
-    void OnMove()
-    {
-        
-    }
-
-
-
 
     void StartJump()
     {
-        //rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.velocity = new Vector2(rb.velocity.x, 0);
         if (_isOnGround)
         {
             _isJumping = true;
             Jump();
         }
-        
-
-
     }
 
-    void EndJump()
-    {
-        _isJumping = false;
-    }
 
+    void EndJump() => _isJumping = false;
+    
+
+    //Resets the posistion of the player for testing
     void OnReset() {
         rb.velocity = new Vector2(0, 0);
         transform.position = new Vector3(0, 0, 0);
@@ -87,6 +72,7 @@ public class PlayerMovment : MonoBehaviour
         //for UI
         MovementVelocity = rb.velocity;
 
+        //Shoots a line from the player to the groundlayer and checks for collision
         _isOnGround =
             Physics2D.Raycast(transform.position + _rayCastOffset, Vector2.down, _rayCastLength, _groundLayer)
             ||
@@ -94,85 +80,57 @@ public class PlayerMovment : MonoBehaviour
 
         Walk();
 
-
-
-
         //Fall multipler used to make fall after apex faster
-        if (rb.velocity.y < 0)
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * _gravityMultiplier * Time.deltaTime;
-        }
+        if (rb.velocity.y < 0) rb.velocity += Vector2.up * Physics2D.gravity.y * _gravityMultiplier * Time.deltaTime;
+        
 
+        //if (_isOnGround) rb.velocity = new Vector2(rb.velocity.x, 0);
 
-
-        if (_isOnGround)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 0);
-        }
-
-
-        if (_isJumping)
-        {
-            Jump();
-            
-        }
+        if (_isJumping) Jump();
+        
 
         animator.SetBool("IsOnGround", _isOnGround);
 
         if (rb.velocity.y > _maxJumpHeight)
         {
             _isJumping = false;
-            //rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.velocity = new Vector2(rb.velocity.x, 0);
         }
     }
 
 
     void Walk()
     {
+        //Read from input device
         Vector2 direction = _inputActions.Player.Move.ReadValue<Vector2>();
 
-        //if (direction.x > 0)
-        //{
-        //    _isFacingRight = true;
-        //}
-        //else if (direction.x < 0)
-        //{
-        //    _isFacingRight = false;
-        //}
-
-        
         _isFacingRight = (direction.x > 0) ? true: false;
         _isFacingRight = (direction.x < 0) ? false : true;
-    
-        
 
+        //Move the Player
         float walking = direction.x * _walkSpeed * Time.deltaTime;
-
         rb.velocity += new Vector2(walking, 0);
 
-
+        //Flip animation if change of direction
         sr.flipX = !_isFacingRight;
 
-        if (rb.velocity.x > _maxWalkSpeed)
-        {
-            rb.velocity = new Vector2(_maxWalkSpeed, rb.velocity.y);
-        }
+        //Clamps X velocity
+        rb.velocity = (rb.velocity.x > _maxWalkSpeed) ?
+            new Vector2(_maxWalkSpeed, rb.velocity.y):
+            new Vector2(rb.velocity.x, rb.velocity.y);
 
+        rb.velocity = (rb.velocity.x < -_maxWalkSpeed) ?
+            new Vector2(-_maxWalkSpeed, rb.velocity.y) :
+            new Vector2(rb.velocity.x, rb.velocity.y);
 
-        if (rb.velocity.x < -_maxWalkSpeed)
-        {
-            rb.velocity = new Vector2(-_maxWalkSpeed, rb.velocity.y);
-        }
-
+        //Changes animation
         animator.SetFloat("Walking_speed", Mathf.Abs(rb.velocity.x));
     }
 
 
-    //void Jump() => rb.AddForce(Vector2.up * _jumpSpeed * Time.deltaTime, ForceMode2D.Impulse);
     void Jump() => rb.velocity += Vector2.up * _jumpSpeed * Time.deltaTime;
     void OnEnable() => _inputActions.Enable();
     void OnDisable() => _inputActions.Disable();
-
 
 
     private void OnDrawGizmos()
@@ -180,6 +138,5 @@ public class PlayerMovment : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position + _rayCastOffset, transform.position + _rayCastOffset + Vector3.down * _rayCastLength);
         Gizmos.DrawLine(transform.position - _rayCastOffset, transform.position - _rayCastOffset + Vector3.down * _rayCastLength);
-
     }
 }
